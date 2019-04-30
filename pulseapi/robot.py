cd296d867dd1ffb34ca24a0be5c05e9fd63e736d
Ilya <grozail@yandex.ru>
2019-04-30 15:27:40 +0300

    add await_stop

--- pulseapi/robot.py
+++ pulseapi/robot.py
@@ -5,6 +5,7 @@ import time
 from pdhttp.api.robot_api import RobotApi
 from pdhttp.models import MotionStatus
 from pulseapi.constants import MT_JOINT
+from pdhttp.rest import ApiException as PulseApiException
 
 
 class RobotPulse(object):
@@ -139,3 +140,13 @@ class RobotPulse(object):
     def await_motion(self, asking_interval=0.1):
         while self.status_motion() != MotionStatus.IDLE:
             time.sleep(asking_interval)
+
+    def await_stop(self, asking_interval=0.1):
+        status = self.status_motion()
+        while status != MotionStatus.IDLE:
+            time.sleep(asking_interval)
+            if (status == MotionStatus.EMERGENCY
+                    or status == MotionStatus.ERROR
+                    or status == MotionStatus.MOTION_FAILED):
+                raise PulseApiException('Failed to await stop: {}'.format(status))
+            status = self.status_motion()
