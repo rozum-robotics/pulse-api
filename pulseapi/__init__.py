e2af1397b705166a38bb6e20836670cd650e8a24
Ilya <grozail@yandex.ru>
2019-07-16 12:23:28 +0300

    reformat imports

--- pulseapi/__init__.py
+++ pulseapi/__init__.py
@@ -1,9 +1,11 @@
 from pdhttp.rest import ApiException as PulseApiException
 from pdhttp import Point, Rotation, MotionStatus
-from pulseapi.environment import (create_box_obstacle,
-                                  create_capsule_obstacle,
-                                  create_plane_obstacle,
-                                  create_simple_capsule_obstacle)
+from pulseapi.environment import (
+    create_box_obstacle,
+    create_capsule_obstacle,
+    create_plane_obstacle,
+    create_simple_capsule_obstacle,
+)
 from pulseapi.constants import MT_JOINT, MT_LINEAR, SIG_HIGH, SIG_LOW
 from pulseapi.robot import RobotPulse
 from pulseapi.utils import pose, position, tool_info, tool_shape, Versions
