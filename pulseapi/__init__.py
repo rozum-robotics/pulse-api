0ad3271b8edbbe3b91cd5d0fb2640f346729961c
Ilya Grozov <ilya.grozov@rozum.com>
2019-10-11 08:30:38 +0000

    Revert "downgrade api because of 1.4.4"
    This reverts commit b62eee0c9def9dd1652539f2f103fbb230822735.

--- pulseapi/__init__.py
+++ pulseapi/__init__.py
@@ -1,12 +1,18 @@
-from __future__ import absolute_import
 from pdhttp.rest import ApiException as PulseApiException
 from pdhttp import Point, Rotation, MotionStatus
-from pulseapi.environment import (create_box_obstacle,
-                                  create_capsule_obstacle,
-                                  create_plane_obstacle,
-                                  create_simple_capsule_obstacle)
+from pulseapi.environment import (
+    create_box_obstacle,
+    create_capsule_obstacle,
+    create_plane_obstacle,
+    create_simple_capsule_obstacle,
+)
 from pulseapi.constants import MT_JOINT, MT_LINEAR, SIG_HIGH, SIG_LOW
 from pulseapi.robot import RobotPulse
 from pulseapi.utils import pose, position, tool_info, tool_shape, Versions
+
+try:
+    from pulseapi.aiorobot import AioRobotPulse
+except ImportError as ie:
+    pass # silent because aiorobot is experimental feature
 
 RestApiException = PulseApiException  # backward compatibility alias
--- pulseapi/environment.py
+++ pulseapi/environment.py
@@ -1,23 +1,26 @@
-from pdhttp import (BoxObstacle, CapsuleObstacle, PlaneObstacle,
-                    SimplifiedCapsuleObstacle)
+from pdhttp import (
+    BoxObstacle,
+    CapsuleObstacle,
+    PlaneObstacle,
+    SimplifiedCapsuleObstacle,
+)
 
 
-def create_box_obstacle(sides, center_position, name='unnamed_box'):
-    return BoxObstacle('BOX', name, sides, center_position)
+def create_box_obstacle(sides, center_position, name="unnamed_box"):
+    return BoxObstacle("BOX", name, sides, center_position)
 
 
-def create_capsule_obstacle(radius,
-                            start_point,
-                            end_point,
-                            name='unnamed_capsule'):
-    return CapsuleObstacle('CAPSULE', name,  radius, start_point, end_point)
+def create_capsule_obstacle(
+    radius, start_point, end_point, name="unnamed_capsule"
+):
+    return CapsuleObstacle("CAPSULE", name, radius, start_point, end_point)
 
 
-def create_plane_obstacle(points, name='unnamed_plane'):
-    return PlaneObstacle('PLANE', name, points)
+def create_plane_obstacle(points, name="unnamed_plane"):
+    return PlaneObstacle("PLANE", name, points)
 
 
 def create_simple_capsule_obstacle(radius, start_point, finish_point):
-    return SimplifiedCapsuleObstacle(radius=radius,
-                                     begin=start_point,
-                                     finish=finish_point)
+    return SimplifiedCapsuleObstacle(
+        radius=radius, begin=start_point, finish=finish_point
+    )
