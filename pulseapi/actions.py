03db879f51e167d1f4ad2e527801a78278e3b36a
Ilya Grozov <ilya.grozov@rozum.com>
2020-02-13 17:52:29 +0300

    [*] fix func name

--- pulseapi/actions.py
+++ pulseapi/actions.py
@@ -14,5 +14,5 @@ def __gripper_action(value: str) -> GripperRobotAction:
 def open_gripper_action() -> GripperRobotAction:
     return __gripper_action("OPEN")
 
-def close_gripper() -> GripperRobotAction:
+def close_gripper_action() -> GripperRobotAction:
     return __gripper_action("CLOSE")
