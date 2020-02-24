9dc4d598672f69847be7594ecd2a29b3408493a9
Ilya Grozov <ilya.grozov@rozum.com>
2020-02-24 17:52:12 +0300

    [+] docstrings for tool helper methods

--- pulseapi/utils.py
+++ pulseapi/utils.py
@@ -76,17 +76,49 @@ def jog(
     Default value, corresponding to absense of the movement: 0. 
     Values MUST belong to [-1;1] range inclusively.
     
-    """
+    :param x: x component of the acceleration vector, defaults to 0
+    :type x: float, optional
+    :param y: y component of the acceleration vector, defaults to 0
+    :type y: float, optional
+    :param z: z component of the acceleration vector, defaults to 0
+    :type z: float, optional
+    :param rx: rx component of the acceleration vector, defaults to 0
+    :type rx: float, optional
+    :param ry: ry component of the acceleration vector, defaults to 0
+    :type ry: float, optional
+    :param rz: rz component of the acceleration vector, defaults to 0
+    :type rz: float, optional
+    :return: accelaration to be used in jogging mode
+    :rtype: JoggingAcceleration
+    """    
     return JoggingAcceleration(
         JoggingAccelerationAcceleration(x, y, z, rx, ry, rz)
     )
 
 
 def tool_info(tcp_position: Position, name: str = "unnamed_tool") -> ToolInfo:
+    """The function creates new TCP to match the properties of an attached or changed work tool. 
+    
+    :param tcp_position: position that defines the distance (in meters) from the 
+    arm's mounting flange center point to the new TCP along the x, y, and z axes accordingly, 
+    rotates the new coordinate system according to the given roll pitch and yaw.
+    :type tcp_position: Position
+    :param name: any random name of the work tool defined by the user (e.g., \\\"gripper\\\"), defaults to "unnamed_tool".
+    :type name: str, optional
+    :return: tool info to be used with change_tool_info() method.
+    :rtype: ToolInfo
+    """    
     return ToolInfo(name=name, tcp=tcp_position)
 
 
 def tool_shape(shape: List[SimplifiedCapsuleObstacle]) -> ToolShape:
+    """The function creates new form of the working tool of the robot, which is used in the calculation of collisions.
+    
+    :param shape: list of simplified capsule obstacles that describe the shape of the tool.
+    :type shape: List[SimplifiedCapsuleObstacle]
+    :return: tool shape to be used with change_tool_shape() method
+    :rtype: ToolShape
+    """    
     return ToolShape(shape=shape)
 
 
