a565a025795a9d42823b928aaffd4d5a63b737a9
Ilya Grozov <ilya.grozov@rozum.com>
2020-05-11 14:55:22 +0300

    [*] make version according to pdhttp

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ DESCRIPTION = "Python API for Pulse Robotic Arm with useful utilities"
 URL = "https://rozum.com"
 EMAIL = "dev@rozum.com"
 AUTHOR = "Rozum Robotics"
-VERSION = "1.8.0.dev1"
+VERSION = "1.8.0.dev2"
 
 if "dev" in VERSION:
     DEVELOPMENT_STATUS = "Development Status :: 4 - Beta"
