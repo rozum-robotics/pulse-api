a3a67ec00aedc95fd1f3446e61d7cbee72017ec2
Ilya Grozov <ilya.grozov@rozum.com>
2020-05-13 17:17:51 +0300

    [*] sonar issues

--- pulseapi/aiorobot.py
+++ pulseapi/aiorobot.py
@@ -5,7 +5,7 @@ from aiopdhttp.api import RobotApi
 from pulseapi import RobotPulse, MotionStatus
 
 class AioRobotPulse(RobotPulse):
-    
+
     def __init__(self, host=None, logger=None):
         self._api = RobotApi()
         if host is not None:
@@ -16,7 +16,7 @@ class AioRobotPulse(RobotPulse):
             logger.addHandler(logging.NullHandler())
         self.logger = logger
         self.host = self._api.api_client.configuration.host
-    
+
     async def __aenter__(self):
         return self
     
@@ -24,6 +24,6 @@ class AioRobotPulse(RobotPulse):
         self.logger.debug(str(asking_interval))
         while await self.status_motion() != MotionStatus.IDLE:
             await asyncio.sleep(asking_interval)
-    
+
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         await self._api.api_client.rest_client.pool_manager.close()
