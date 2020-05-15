812053f9cbcefadf79c08b4b91bed93efafc625e
Ilya Grozov <ilya.grozov@rozum.com>
2020-05-15 14:30:06 +0300

    [*] docstring length

--- pulseapi/utils.py
+++ pulseapi/utils.py
@@ -28,17 +28,22 @@ def position(
 ) -> Position:
     """Creates position motion target.
 
-    Use this method to create positions which will be passed to set_position and run_positions methods of RobotPulse.
+    Use this method to create positions which will be passed to set_position
+    and run_positions methods of RobotPulse.
 
-    Example: there is need to move robot's TCP to point with coordinates x=0.3m, y=0.2m, z=0.1m
-    and look down vertically relative to base. Call _position((0.3, 0.2, 0.1), (3.1415, 0, 0))_ and pass result to one
+    Example: there is need to move robot's TCP to point with coordinates
+    x=0.3m, y=0.2m, z=0.1m and look down vertically relative to base.
+    Call _position((0.3, 0.2, 0.1), (3.1415, 0, 0))_ and pass result to one
     of the methods mentioned earlier.
 
-    :param point: list or tupple containing x, y, z coordinates (in meters) where robot should move its TCP
+    :param point: list or tupple containing x, y, z coordinates (in meters)
+    where robot should move its TCP
     :type point: Iterable[float]
-    :param rotation: list or tupple containing roll, pitch, yaw coordinates (in radians) for TCP
+    :param rotation: list or tupple containing roll, pitch, yaw coordinates
+    (in radians) for TCP
     :type rotation: Iterable[float]
-    :param actions: list containing actions from pulseapi.actions module, defaults to None
+    :param actions: list containing actions from pulseapi.actions module,
+    defaults to None
     :type actions: Optional[ActionsList], optional
     :return: position motion target
     :rtype: Position
@@ -51,11 +56,14 @@ def pose(
 ) -> Pose:
     """Creates pose motion target.
 
-    Use this method to create poses which will be passed to set_pose and run_poses methods of RobotPulse.
+    Use this method to create poses which will be passed to set_pose and
+    run_poses methods of RobotPulse.
 
-    :param angles: list or tupple containing 6 angles for motors (in degrees). Order: base-0th, tcp-5th
+    :param angles: list or tupple containing 6 angles for motors (in degrees).
+    Order: base-0th, tcp-5th
     :type angles: Iterable[float]
-    :param actions:  list containing actions from pulseapi.actions module, defaults to None
+    :param actions: list containing actions from pulseapi.actions module,
+    defaults to None
     :type actions: Optional[ActionsList], optional
     :return: pose motion target
     :rtype: Pose
@@ -73,8 +81,9 @@ def jog(
 ) -> JoggingAcceleration:
     """Creates motion target for jogging mode.
 
-    Jogging acceleration is a six-component vector ('x', 'y', 'z', 'rx', 'ry', 'rz').
-    Components are optional and relative to the base coordinate system of the robotic arm.
+    Jogging acceleration is a six-component vector
+    ('x', 'y', 'z', 'rx', 'ry', 'rz'). Components are optional and relative to
+    the base coordinate system of the robotic arm.
     Default value, corresponding to absense of the movement: 0.
     Values MUST belong to [-1;1] range inclusively.
 
@@ -99,13 +108,16 @@ def jog(
 
 
 def tool_info(tcp_position: Position, name: str = "unnamed_tool") -> ToolInfo:
-    """Creates new TCP to match the properties of an attached or changed work tool.
+    """Creates new TCP to match the properties of an attached or changed work
+    tool.
 
     :param tcp_position: position that defines the distance (in meters) from the
-    arm's mounting flange center point to the new TCP along the x, y, and z axes accordingly,
-    rotates the new coordinate system according to the given roll pitch and yaw.
+    arm's mounting flange center point to the new TCP along the x, y, and z axes
+    accordingly, rotates the new coordinate system according to the given roll,
+    pitch and yaw.
     :type tcp_position: Position
-    :param name: any random name of the work tool defined by the user (e.g., \\\"gripper\\\"), defaults to "unnamed_tool".
+    :param name: any random name of the work tool defined by the user
+    (e.g., \"gripper\"), defaults to "unnamed_tool".
     :type name: str, optional
     :return: tool info to be used with change_tool_info() method.
     :rtype: ToolInfo
@@ -114,9 +126,11 @@ def tool_info(tcp_position: Position, name: str = "unnamed_tool") -> ToolInfo:
 
 
 def tool_shape(shape: List[SimplifiedCapsuleObstacle]) -> ToolShape:
-    """Creates new form of the working tool of the robot, which is used in the calculation of collisions.
+    """Creates new form of the working tool of the robot, which is used in the
+    calculation of collisions.
 
-    :param shape: list of simplified capsule obstacles that describe the shape of the tool.
+    :param shape: list of simplified capsule obstacles that describe the shape
+    of the tool.
     :type shape: List[SimplifiedCapsuleObstacle]
     :return: tool shape to be used with change_tool_shape() method
     :rtype: ToolShape
